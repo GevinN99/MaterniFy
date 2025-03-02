ba4e928239c6bede3ae26b62726b24d11e74f9af
LeharaJaya <leharachamee20@gmail.com>
2025-03-02 10:22:00 +0530

    Update Index.jsx

--- frontend/app/(tabs)/index.jsx
+++ frontend/app/(tabs)/index.jsx
@@ -1,6 +1,11 @@
 import React from "react"
 import { View, Text } from "react-native"
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HealthPlanScreen from "./HealthPlanScreen";
+import AiGeneratedHealthTipsScreen from "./AiGeneratedHealthTipsScreen";
+import DailyHealthChecklistScreen from "./DailyHealthChecklistScreen";
 
+const Tab = createBottomTabNavigator();
 function home() {
 	return (
 		<View>
@@ -8,5 +13,14 @@ function home() {
 		</View>
 	)
 }
+export default function App() {
+	return (
+	  <Tab.Navigator initialRouteName="HealthPlan" screenOptions={{ headerShown: false }}>
+		<Tab.Screen name="HealthPlan" component={HealthPlanScreen} options={{ tabBarLabel: 'Health Plan' }} />
+		<Tab.Screen name="AiTips" component={AiGeneratedHealthTipsScreen} options={{ tabBarLabel: 'AI Tips' }} />
+		<Tab.Screen name="Checklist" component={DailyHealthChecklistScreen} options={{ tabBarLabel: 'Checklist' }} />
+	  </Tab.Navigator>
+	);
+  }
 
-export default home
+
