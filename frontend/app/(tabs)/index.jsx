841a939ddef54997dc0aec7cca58ee4aab6cd0c7
Gevin Nanayakkara <gevinnanayakkara@gmail.com>
2025-03-12 08:06:08 +0530

    Bug fix

--- frontend/app/(tabs)/index.jsx
+++ frontend/app/(tabs)/index.jsx
@@ -1,4 +1,3 @@
-export default home;
 import React, { useState } from "react";
 import {
   View,
@@ -11,27 +10,25 @@ import {
 } from "react-native";
 import { useRouter } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
-import { router } from "expo-router";
 import moment from "moment";
 import { Svg, Circle } from "react-native-svg";
 import { LinearGradient } from "expo-linear-gradient";
 import { LineChart } from "react-native-chart-kit";
 import { Dimensions } from "react-native";
 
-
 const Landing = () => {
   const [selectedEmotion, setSelectedEmotion] = useState(null);
   const [selectedDate, setSelectedDate] = useState(moment().format("YYYY-MM-DD"));
   const [currentWeek, setCurrentWeek] = useState(moment());
   const [scores, setScores] = useState([5, 10, 15]); // Dummy scores for testing
-  
-	const router = useRouter();
+
+  const router = useRouter();
 
   const screenWidth = Dimensions.get("window").width;
 
   const handleSelectEmotion = (emotion) => {
     setSelectedEmotion(emotion);
-    console.log('Selected Emotion: ${emotion}');
+    console.log(`Selected Emotion: ${emotion}`);
   };
 
   const getWeekDates = () => {
@@ -52,168 +49,164 @@ const Landing = () => {
     const strokeWidth = 10;
     const circumference = 2 * Math.PI * radius;
     const progress = (percentage / 100) * circumference;
-  
+
     return (
-      <View style={{ justifyContent: "center", alignItems: "center" }}>
-        <Svg width={100} height={100}>
-          <Circle cx="50" cy="50" r={radius} stroke="#E0E0E0" strokeWidth={strokeWidth} fill="none" />
-          <Circle
-            cx="50"
-            cy="50"
-            r={radius}
-            stroke="orange"
-            strokeWidth={strokeWidth}
-            fill="none"
-            strokeDasharray={circumference}
-            strokeDashoffset={circumference - progress}
-            strokeLinecap="round"
-          />
-        </Svg>
-        <View style={{ position: "absolute", justifyContent: "center", alignItems: "center" }}>
-          <Text style={{ fontSize: 16, fontWeight: "bold" }}>{percentage}%</Text>
+        <View style={{ justifyContent: "center", alignItems: "center" }}>
+          <Svg width={100} height={100}>
+            <Circle cx="50" cy="50" r={radius} stroke="#E0E0E0" strokeWidth={strokeWidth} fill="none" />
+            <Circle
+                cx="50"
+                cy="50"
+                r={radius}
+                stroke="orange"
+                strokeWidth={strokeWidth}
+                fill="none"
+                strokeDasharray={circumference}
+                strokeDashoffset={circumference - progress}
+                strokeLinecap="round"
+            />
+          </Svg>
+          <View style={{ position: "absolute", justifyContent: "center", alignItems: "center" }}>
+            <Text style={{ fontSize: 16, fontWeight: "bold" }}>{percentage}%</Text>
+          </View>
         </View>
-      </View>
     );
   };
-  
 
   return (
-    <SafeAreaView style={styles.safeContainer}>
-      <ScrollView contentContainerStyle={styles.container}>
-        <Image source={require("../../assets/images/landing.png")} style={styles.image} /> 
-        <Text style={styles.title}>Welcome, Sarah!</Text>
-        <Text style={styles.subtitle}>How are you feeling today?</Text>
+      <SafeAreaView style={styles.safeContainer}>
+        <ScrollView contentContainerStyle={styles.container}>
+          <Image source={require("../../assets/images/landing.png")} style={styles.image} />
+          <Text style={styles.title}>Welcome, Sarah!</Text>
+          <Text style={styles.subtitle}>How are you feeling today?</Text>
 
-        <View style={styles.emojiContainer}>
-          {[{ name: require("../../assets/images/sunglasses.png"), label: "Happy"  },
-            { name: require("../../assets/images/smile.png"), label: "Calm"},
-            { name: require("../../assets/images/thinking.png"), label: "Confused"},
-            { name: require("../../assets/images/sad.png"), label: "Sad" },
-            { name: require("../../assets/images/angry.png"), label: "Angry" },
-          ].map(({ name, label, color }) => (
-            <TouchableOpacity key={label} onPress={() => handleSelectEmotion(label)}>
-      <Image
-        source={name}
-        style={[
-          styles.emojiImage,
-          { opacity: selectedEmotion === label ? 1 : 0.5 }, // Highlight selected emoji
-        ]}
-      />
-    </TouchableOpacity>
-          ))}
-        </View>
-
-        <View style={styles.secContainer}>
-          <View style={styles.row}>
-            <TouchableOpacity onPress={() => router.push("/getstart")}>
-              <Image source={require("../../assets/images/medical-report.png")} style={styles.sectors} />
-              <Text style={styles.topic}>Health Plan</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => router.push("/getstart")}>
-              <Image source={require("../../assets/images/mental-health (1).png")} style={styles.sectors} />
-              <Text style={styles.topic}>Mental Health</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.row}>
-            <TouchableOpacity onPress={() => router.push("/getstart")}>
-              <Image source={require("../../assets/images/insurance-policy.png")} style={styles.sectors} />
-              <Text style={styles.topic}>Appointments</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => router.push("/getstart")}>
-              <Image source={require("../../assets/images/first-aid-kit.png")} style={styles.sectors} />
-              <Text style={styles.topic}>Emergency</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-
-        <View style={styles.calendarContainer}>
-          <View style={styles.weekHeader}>
-            <TouchableOpacity onPress={handlePrevWeek}>
-              <Ionicons name="chevron-back" size={24} color="#64A8F1" />
-            </TouchableOpacity>
-            <Text style={styles.weekTitle}>{currentWeek.format("  MMMM D  ")}</Text>
-            <TouchableOpacity onPress={handleNextWeek}>
-              <Ionicons name="chevron-forward" size={24} color="#64A8F1" />
-            </TouchableOpacity>
-          </View>
-          <View style={styles.weekDays}>
-            {getWeekDates().map((date) => (
-              <TouchableOpacity key={date.format("YYYY-MM-DD")} onPress={() => setSelectedDate(date.format("YYYY-MM-DD"))}>
-                <Text style={[styles.dayText, selectedDate === date.format("YYYY-MM-DD") && styles.selectedDay]}>
-                {date.format("dd").charAt(0)}{"\n"}{"\n"}{date.format("D")}
-                </Text>
-              </TouchableOpacity>
+          <View style={styles.emojiContainer}>
+            {[{ name: require("../../assets/images/sunglasses.png"), label: "Happy"  },
+              { name: require("../../assets/images/smile.png"), label: "Calm"},
+              { name: require("../../assets/images/thinking.png"), label: "Confused"},
+              { name: require("../../assets/images/sad.png"), label: "Sad" },
+              { name: require("../../assets/images/angry.png"), label: "Angry" },
+            ].map(({ name, label, color }) => (
+                <TouchableOpacity key={label} onPress={() => handleSelectEmotion(label)}>
+                  <Image
+                      source={name}
+                      style={[
+                        styles.emojiImage,
+                        { opacity: selectedEmotion === label ? 1 : 0.5 }, // Highlight selected emoji
+                      ]}
+                  />
+                </TouchableOpacity>
             ))}
           </View>
-          {selectedDate && (
-            <><Text style={styles.dateText}>Doctor Checkup </Text><Text>{selectedDate}</Text></>
-          )}
-        </View>
-        <LinearGradient 
-          colors={['#E2E0E0', '#64A8F1']} 
-          style={styles.healthplan}>
-          <Text style={styles.healthtitle}>
-            Today's Health plan
-          </Text>
-          <Text style={styles.texthealth}>
-          "You need 8 glasses of water today 💧"
-          </Text>
-          <Text style={styles.texthealth}>
-          "Remember to take your iron supplements"
-          </Text>
-          <Text style={styles.texthealth}>
-          "Mild back pain detected? Try 5 mins of stretching."
-          </Text>
-          <CircularProgress percentage={100} />
-          
-      </LinearGradient>
 
-      <View style={{ alignItems: "center", padding: 20 }}>
-          <Text style={{ fontSize: 20, fontWeight: "bold", marginBottom: 10 }}>
-            Mental Health Summary
-          </Text>
-
-          <View style={{ marginTop: 5, marginBottom:20 }}>
-          <LineChart
-            data={{
-              labels: ["Last month", "Jan 26 - Feb 1", "Today"],
-              datasets: [{ data: scores }],
-            }}
-            width={screenWidth * 0.9}
-            height={250}
-            yAxisLabel=""
-            chartConfig={{
-              backgroundColor: "#E3F2FD",
-              backgroundGradientFrom: "#E3F2FD",
-              backgroundGradientTo: "#BBDEFB",
-              decimalPlaces: 0,
-              color: (opacity = 1) => `rgba(33, 150, 243, ${opacity})`,
-              labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-
-              style: { borderRadius: 16 },
-              propsForDots: { r: "5", strokeWidth: "2", stroke: "#2196F3" },
-            }}
-            bezier
-            style={{ borderRadius: 16 }}
-          />
-          
-            <Text style={{ fontSize: 18, textAlign:"center" }}>
-              ⚠ {getRiskMessage(scores[scores.length - 1])}
-            </Text>
+          <View style={styles.secContainer}>
+            <View style={styles.row}>
+              <TouchableOpacity onPress={() => router.push("/getstart")}>
+                <Image source={require("../../assets/images/medical-report.png")} style={styles.sectors} />
+                <Text style={styles.topic}>Health Plan</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => router.push("/getstart")}>
+                <Image source={require("../../assets/images/mental-health (1).png")} style={styles.sectors} />
+                <Text style={styles.topic}>Mental Health</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.row}>
+              <TouchableOpacity onPress={() => router.push("/getstart")}>
+                <Image source={require("../../assets/images/insurance-policy.png")} style={styles.sectors} />
+                <Text style={styles.topic}>Appointments</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => router.push("/getstart")}>
+                <Image source={require("../../assets/images/first-aid-kit.png")} style={styles.sectors} />
+                <Text style={styles.topic}>Emergency</Text>
+              </TouchableOpacity>
+            </View>
           </View>
-        </View>
 
-        // -Karunya-
-        <View className="flex-1 items-center justify-center bg-gray-100 px-6">
-			    <Text className="text-2xl font-bold text-gray-800 mb-6">Welcome to Maternify!</Text>
-			    <TouchableOpacity className="w-full bg-blue-300 p-6 rounded-xl shadow-md items-center" onPress={() => router.push("epds")}>
-				    <Text className="text-xl font-bold text-white">EPDS Assessment</Text>
-				    <Text className="text-sm text-white">Take the Edinburgh Postnatal Depression Scale</Text>
-			    </TouchableOpacity>
-		    </View>
+          <View style={styles.calendarContainer}>
+            <View style={styles.weekHeader}>
+              <TouchableOpacity onPress={handlePrevWeek}>
+                <Ionicons name="chevron-back" size={24} color="#64A8F1" />
+              </TouchableOpacity>
+              <Text style={styles.weekTitle}>{currentWeek.format("  MMMM D  ")}</Text>
+              <TouchableOpacity onPress={handleNextWeek}>
+                <Ionicons name="chevron-forward" size={24} color="#64A8F1" />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.weekDays}>
+              {getWeekDates().map((date) => (
+                  <TouchableOpacity key={date.format("YYYY-MM-DD")} onPress={() => setSelectedDate(date.format("YYYY-MM-DD"))}>
+                    <Text style={[styles.dayText, selectedDate === date.format("YYYY-MM-DD") && styles.selectedDay]}>
+                      {date.format("dd").charAt(0)}{"\n"}{"\n"}{date.format("D")}
+                    </Text>
+                  </TouchableOpacity>
+              ))}
+            </View>
+            {selectedDate && (
+                <><Text style={styles.dateText}>Doctor Checkup </Text><Text>{selectedDate}</Text></>
+            )}
+          </View>
+          <LinearGradient
+              colors={['#E2E0E0', '#64A8F1']}
+              style={styles.healthplan}>
+            <Text style={styles.healthtitle}>
+              Today's Health plan
+            </Text>
+            <Text style={styles.texthealth}>
+              "You need 8 glasses of water today 💧"
+            </Text>
+            <Text style={styles.texthealth}>
+              "Remember to take your iron supplements"
+            </Text>
+            <Text style={styles.texthealth}>
+              "Mild back pain detected? Try 5 mins of stretching."
+            </Text>
+            <CircularProgress percentage={100} />
+          </LinearGradient>
 
-      </ScrollView>
-    </SafeAreaView>
+          <View style={{ alignItems: "center", padding: 20 }}>
+            <Text style={{ fontSize: 20, fontWeight: "bold", marginBottom: 10 }}>
+              Mental Health Summary
+            </Text>
+
+            <View style={{ marginTop: 5, marginBottom:20 }}>
+              <LineChart
+                  data={{
+                    labels: ["Last month", "Jan 26 - Feb 1", "Today"],
+                    datasets: [{ data: scores }],
+                  }}
+                  width={screenWidth * 0.9}
+                  height={250}
+                  yAxisLabel=""
+                  chartConfig={{
+                    backgroundColor: "#E3F2FD",
+                    backgroundGradientFrom: "#E3F2FD",
+                    backgroundGradientTo: "#BBDEFB",
+                    decimalPlaces: 0,
+                    color: (opacity = 1) => `rgba(33, 150, 243, ${opacity})`,
+                    labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+                    style: { borderRadius: 16 },
+                    propsForDots: { r: "5", strokeWidth: "2", stroke: "#2196F3" },
+                  }}
+                  bezier
+                  style={{ borderRadius: 16 }}
+              />
+              <Text style={{ fontSize: 18, textAlign:"center" }}>
+                ⚠ {getRiskMessage(scores[scores.length - 1])}
+              </Text>
+            </View>
+          </View>
+
+          {/* -Karunya- */}
+          <View className="flex-1 items-center justify-center bg-gray-100 px-6">
+            <Text className="text-2xl font-bold text-gray-800 mb-6">Welcome to Maternify!</Text>
+            <TouchableOpacity className="w-full bg-blue-300 p-6 rounded-xl shadow-md items-center" onPress={() => router.push("epds")}>
+              <Text className="text-xl font-bold text-white">EPDS Assessment</Text>
+              <Text className="text-sm text-white">Take the Edinburgh Postnatal Depression Scale</Text>
+            </TouchableOpacity>
+          </View>
+
+        </ScrollView>
+      </SafeAreaView>
   );
 };
 
@@ -325,7 +318,6 @@ const styles = StyleSheet.create({
     fontSize:20,
     fontWeight:"bold",
     padding:10,
-
   },
   texthealth:{
     textAlign:"center",
@@ -336,7 +328,6 @@ const styles = StyleSheet.create({
     height: 40,
     resizeMode: "contain",
   },
-  
 });
 
 export default Landing;
