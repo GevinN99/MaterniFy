0facd83d6f814b4494e3e3f561dfd945a287e092
Karu-0503 <163737447+Karu-0503@users.noreply.github.com>
2025-03-13 13:58:33 +0530

    version changes

--- frontend/app/(tabs)/index.jsx
+++ frontend/app/(tabs)/index.jsx
@@ -1,4 +1,3 @@
-//export default home;
 import React, { useState } from "react";
 import {
   View,
@@ -197,14 +196,14 @@ const Landing = () => {
             </View>
           </View>
 
-        {/* -Karunya- */}
-        <View className="flex-1 items-center justify-center bg-gray-100 px-6">
-			    <Text className="text-2xl font-bold text-gray-800 mb-6">Welcome to Maternify!</Text>
-			    <TouchableOpacity className="w-full bg-blue-300 p-6 rounded-xl shadow-md items-center" onPress={() => router.push("epds")}>
-				    <Text className="text-xl font-bold text-white">EPDS Assessment</Text>
-				    <Text className="text-sm text-white">Take the Edinburgh Postnatal Depression Scale</Text>
-			    </TouchableOpacity>
-		    </View>
+          {/* -Karunya- */}
+          <View className="flex-1 items-center justify-center bg-gray-100 px-6">
+            <Text className="text-2xl font-bold text-gray-800 mb-6">Welcome to Maternify!</Text>
+            <TouchableOpacity className="w-full bg-blue-300 p-6 rounded-xl shadow-md items-center" onPress={() => router.push("epds")}>
+              <Text className="text-xl font-bold text-white">EPDS Assessment</Text>
+              <Text className="text-sm text-white">Take the Edinburgh Postnatal Depression Scale</Text>
+            </TouchableOpacity>
+          </View>
 
         </ScrollView>
       </SafeAreaView>
